58c42ed051fc0fc6c99c58880a39ad447639b629
Zudikhin <uzudihin@gmail.com>
2024-05-21 18:38:58 +0300

    legs

--- js/main.js
+++ js/main.js
@@ -1,9 +1,27 @@
 $(document).ready(function() {
     "use strict";
 
-    $(".methodology_accordion_list_item_head").click(function() {
+    $(".against_accordion_list_item_head").click(function() {
         $(this).toggleClass("active");
         $(this).siblings().slideToggle()
+    });
+
+    $('.against_review_block_slider').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true,
+        appendDots: $(".against_review_block_dots"),
+        prevArrow: $(".against_review_block_arrows_prev"),
+        nextArrow: $(".against_review_block_arrows_next"),
+        responsive: [
+          {
+            breakpoint: 991,
+            settings: {
+              slidesToShow: 1
+            }
+          }
+        ]
     });
 
 });
