58ab081e3bd8f414b0455a09d0ce231b29091358
Zudikhin <uzudihin@gmail.com>
2024-07-18 00:46:51 +0300

    fuh

--- js/main.js
+++ js/main.js
@@ -1,12 +1,10 @@
 $(document).ready(function() {
     "use strict";
 
-    $(".walk_pain_main_points_hero_item button").click(function() {
-      var attr = $(this).attr("data-id");
-      $(".walk_pain_main_points_hero_item button").removeClass("active");
-      $(this).addClass("active");
-      $(".walk_pain_main_points_mob_item").removeClass("active");
-      $(`#${attr}`).addClass("active");
-    });
-
+    $('.open-popup').magnificPopup({  
+        type: 'inline',
+        preloader: false,
+        focus: '#hidden-content',
+        mainClass: 'mfp-fade',
+    });  
 });
